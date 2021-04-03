0e34288ed10aa54a4c86f595264d3f7070c43efa
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-04-03 21:53:20 +0200

    basic Basket created

--- src/main/java/com/shopcart/shoppingcart/model/Basket.java
+++ src/main/java/com/shopcart/shoppingcart/model/Basket.java
@@ -1,4 +1,34 @@
 package com.shopcart.shoppingcart.model;
 
+import lombok.ToString;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+@ToString
 class Basket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+
+    ArrayList<Item> content;
+    BigDecimal totalPrice;
+
+    private Basket(ArrayList<Item> content) {
+        this.content = content;
+        this.totalPrice = calculateTotalPrice(content);
+    }
+
+    private static BigDecimal calculateTotalPrice(ArrayList<Item> content) {
+        BigDecimal sum = new BigDecimal("0");
+        for (Item item : content) {
+            sum = sum.add(item.price);
+        }
+        return sum;
+    }
+
+
 }
